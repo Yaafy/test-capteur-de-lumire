51bbce2b28dfc8fc9bcfdafe8a044e77c752ef74
Yaafy <132677932+Yaafy@users.noreply.github.com>
2023-05-11 14:44:11 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,50 @@
 basic.forever(function () {
-    basic.showNumber(input.lightLevel())
+    if (input.lightLevel() < 43) {
+    	
+    }
+    if (input.lightLevel() > 42 && input.lightLevel() < 86) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    }
+    if (input.lightLevel() > 85 && input.lightLevel() < 129) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (input.lightLevel() > 128 && input.lightLevel() < 172) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (input.lightLevel() > 171 && input.lightLevel() < 215) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (input.lightLevel() > 214) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
 })
