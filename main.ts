df23666081a7e341a534c00cab9ad15e7e58ab5a
Yaafy <132677932+Yaafy@users.noreply.github.com>
2023-05-12 15:34:42 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,50 +1,6 @@
 basic.forever(function () {
-    if (input.lightLevel() < 43) {
-    	
-    }
-    if (input.lightLevel() > 42 && input.lightLevel() < 86) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            # # # # #
-            `)
-    }
-    if (input.lightLevel() > 85 && input.lightLevel() < 129) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            # # # # #
-            # # # # #
-            `)
-    }
-    if (input.lightLevel() > 128 && input.lightLevel() < 172) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-    }
-    if (input.lightLevel() > 171 && input.lightLevel() < 215) {
-        basic.showLeds(`
-            . . . . .
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-    }
-    if (input.lightLevel() > 214) {
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-    }
+    led.plotBarGraph(
+    input.lightLevel(),
+    255
+    )
 })
